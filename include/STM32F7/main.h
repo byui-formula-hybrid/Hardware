58fec60eb052232182e4bbe5620c936661a8897d
Porter McGary <porter.mcgary@astrobytes.me>
2026-03-31 17:25:32 -0600

    Integrate with ESP32S3 (#12)
    * Create Board JSON
    
    * feat: integrate ESP32-S3 support with configuration and source files
    
    * feat: add platformio configuration files for ESP32-S3 and STM32F7 support
    
    * fix: update include path for ESP32-S3 build flags in platformio_esp32s3.ini
    
    * fix: update platformio configuration to ignore FreeRTOS and clean up build flags

--- include/STM32F7/main.h
+++ include/STM32F7/main.h
@@ -6,17 +6,20 @@ extern "C" {
 #endif
 
 #if defined(ENV_STM32)
+
 #if defined(ENV_STM32F7)
+
 #include "stm32f7defs.h"
+
 #endif // ENV_STM32F7
 
-// TODO: Coment
+// TODO: Comment
 void Error_Handler(void);
 
 #endif // ENV_STM32
 
 #ifdef __cplusplus
 }
-#endif
+#endif // extern "C"
 
 #endif // MAIN_H
